087baf50673872b7c90af6f23e4b7506904c0781
ej84 <ej84@njit.edu>
2021-03-17 12:47:49 -0400

    created CSV Reader class.

---
+++ src/CSVReader.js
@@ -0,0 +1,26 @@
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+
+class CSVReader{
+    static CSV_Read(filePath, modelType){
+        const options = {
+            columns: true,
+            delimiter: ',',
+            trim: true,
+            skip_empty_lines:true
+
+        };
+        let fileContents = fs.readFileSync(filePath);
+        const records = parse(fileContents, options);
+        let list = Array();
+        records.forEach(function (data){
+            list.push(modelType.create(data));
+        })
+
+        return list;
+
+    }
+
+}
+
+module.exports = CSVReader;
